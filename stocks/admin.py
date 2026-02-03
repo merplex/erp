d0b682fc762c7c8e0a81d8f50ba645e7eda8e7be
merplex <premsak.c@gmail.com>
2026-02-03 10:45:47 +0000

    m6

--- stocks/admin.py
+++ stocks/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    # กำหนดคอลัมน์ที่จะโชว์ในหน้าลิสต์
+    list_display = ('name', 'sku', 'price', 'sale_price', 'stock_quantity', 'unit')
+    # เพิ่มช่องค้นหา
+    search_fields = ('name', 'sku', 'barcode')
