1ae233c807dd7b35dc5ee61d8cb3f6a095b43074
merplex <premsak.c@gmail.com>
2026-02-08 17:45:19 +0000

    3

--- stocks/admin.py
+++ stocks/admin.py
@@ -1526,20 +1526,20 @@ class SalesReportAdmin(admin.ModelAdmin):
         now = timezone.now()
         
         # กรองสถานะที่ปิดใบงานแล้ว
-        date_query = Q(salesitem__sales_order__status__in=['Shipped', 'Completed'])
+        date_query = Q(sales_items__sales_order__status__in=['Shipped', 'Completed'])
         
         if period == '1year':
-            date_query &= Q(salesitem__sales_order__order_date__year=now.year)
+            date_query &= Q(sales_items__sales_order__order_date__year=now.year)
         elif period == '4months':
-            date_query &= Q(salesitem__sales_order__order_date__gte=now - timedelta(days=120))
+            date_query &= Q(sales_items__sales_order__order_date__gte=now - timedelta(days=120))
         elif period == '1month':
-            date_query &= Q(salesitem__sales_order__order_date__gte=now - timedelta(days=30))
+            date_query &= Q(sales_items__sales_order__order_date__gte=now - timedelta(days=30))
 
         # คำนวณรายบรรทัด
         return qs.annotate(
-            total_qty=Sum('salesitem__quantity_shipped', filter=date_query),
+            total_qty=Sum('sales_items__quantity_shipped', filter=date_query),
             total_sales_val=Sum(
-                F('salesitem__sale_price') * F('salesitem__quantity_shipped'), 
+                F('sales_items__sale_price') * F('sales_items__quantity_shipped'), 
                 filter=date_query,
                 output_field=DecimalField()
             )
