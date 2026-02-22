a357afb12f63941ac0fa2fb264d676405c8aa509
merplex <premsak.c@gmail.com>
2026-02-22 12:23:55 +0000

    4

--- stocks/models.py
+++ stocks/models.py
@@ -4,6 +4,7 @@ from django.db.models import Sum
 from django.db.models.signals import post_delete
 from django.core.exceptions import ValidationError
 from django.utils import timezone
+from django.utils.safestring import mark_safe
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
